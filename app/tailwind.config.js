1bb00c2a78575a8999a64c5a008c946f61125a6f
Jakob Nacanaynay <jnac8080@gmail.com>
2023-03-31 16:38:48 +0000

    Add font and color config

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      'pink': '#cc4365',
+      'magenta': '#872e62',
+      'purple': '#42195e',
+      'black': '#0f1020',
+      'white': '#f9f7f1',
+    },
+    fontFamily: {
+      'heading': ['Bebas Neue', 'cursive'],
+      'paragraph': ['Barlow', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
