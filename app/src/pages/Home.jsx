b378e5e35132927f6176bd7c270caff87a6fb447
Asher-W <85578685+Asher-W@users.noreply.github.com>
2023-04-01 18:35:07 -0700

    better animation

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import peanuts from '../images/roasted-peanuts-jpg-1656535069.jpg';
 
 const Home = () => {
     return <h1 className="text-3xl font-bold underline">
-      <Menu />
+      <NavBar openMenu={()=>{console.log("clicked")}} />
       <Title image={peanuts} />
       <Slide1 image={peanuts} />
       <Slide1 image={peanuts} />
@@ -28,12 +28,14 @@ function Title(props){
     </div>
   )
 }
-function Menu(props){
+function NavBar(props){
   return(
     <div className='fixed w-screen h-12 z-10 flex justify-between flex-none px-4 top-2 min-w-slide'>
-      <p className='text-white text-4xl'>=</p>
+      <p className='text-white text-4xl' onClick={props.openMenu()}>=</p>
       <p className='text-white text-4xl'>Logo</p>
-      <p className='border-4 border-white px-1 hover:border-pink active:text-pink transition-all text-white text-xl'><span className='align-middle'>FLY WITH US</span></p>
+      <p className='px-3 active:text-pink transition-all text-white text-xl relative hover:before:clip-in before:clip-out before:border-4 before:border-white flex justify-center items-center'>
+        <span className='align-middle inline-block'>FLY WITH US</span>
+      </p>
     </div>
   )
 }
