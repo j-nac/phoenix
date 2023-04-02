09e4b060ddaf9b782e44cac05d5a267fd7ec2f0f
Asher-W <85578685+Asher-W@users.noreply.github.com>
2023-04-01 19:07:32 -0700

    better transition

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import '../styles/home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import peanuts from '../images/roasted-peanuts-jpg-1656535069.jpg';
 
 const Home = () => {
@@ -31,7 +33,7 @@ function Title(props){
 function NavBar(props){
   return(
     <div className='fixed w-screen h-12 z-10 flex justify-between flex-none px-4 top-2 min-w-slide'>
-      <p className='text-white text-4xl' onClick={props.openMenu()}>=</p>
+      <p className='text-white text-4xl' onClick={props.openMenu()}><FontAwesomeIcon icon={faBars}/></p>
       <p className='text-white text-4xl'>Logo</p>
       <p className='px-3 active:text-pink transition-all text-white text-xl relative hover:before:clip-in active:before:border-pink before:clip-out before:border-4 before:border-white flex justify-center items-center'>
         <span className='align-middle inline-block'>FLY WITH US</span>
