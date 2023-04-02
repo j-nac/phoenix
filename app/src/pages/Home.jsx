0d600263c999e5a3f4912617e457d0523bde7173
Asher-W <85578685+Asher-W@users.noreply.github.com>
2023-04-02 14:28:30 -0700

    more animations

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -45,7 +45,7 @@ function NavBar(props){
     <div className='fixed w-screen h-12 z-50 flex justify-between flex-none px-16 top-2 min-w-slide'>
       <p className='text-white text-4xl hover:text-pink transition-all' onClick={props.openMenu}><FontAwesomeIcon icon={faBars} id="open"/><FontAwesomeIcon icon={faX} id="close" className="hide"/></p>
       <a className='text-white text-4xl' href="#root">Logo</a>
-      <p className='px-3 transition-all text-white text-xl relative hover:text-pink hover:before:clip-in before:clip-out before:bg-white flex justify-center items-center before:rounded-sm'>
+      <p className='px-1 transition-all text-white text-lg relative hover:text-pink hover:before:clip-in before:clip-out before:bg-white flex justify-center items-center before:rounded-sm'>
         <span className='relative z-60 align-middle inline-block'>FLY WITH US</span>
       </p>
     </div>
@@ -56,9 +56,14 @@ function Menu(props){
   return(
     <>
       <div className='fixed h-screen w-screen bg-black min-w-slide z-20 backdrop-blur-sm bg-opacity-75 closed' id="menu-back" onClick={props.clicked}> </div>
-      <div className='fixed h-screen w-1/3 bg-purple z-30 flex items-center justify-end min-w-menu closed' id="menu">
-        <div className='h-3/4 bg-white flex items-center w-3/4'>
-
+      <div className='fixed h-screen w-1/3 bg-purple z-30 flex justify-end min-w-menu closed' id="menu">
+        <div className='h-2/3 flex justify-between flex-col w-3/4 mt-16'>
+          <p className='hover:before:[content:"🡆"] hover:before:opacity-100 before:opacity-0 before:w-8 before:inline-block text-white before:transition-all before:relative before:right-10 hover:before:right-0 after:block after:h-1 after:w-3/4 after:bg-white hover:text-pink transition-color'>Home</p>
+          <p className='hover:before:[content:"🡆"] hover:before:opacity-100 before:opacity-0 before:w-8 before:inline-block text-white before:transition-all before:relative before:right-10 hover:before:right-0 after:block after:h-1 after:w-3/4 after:bg-white hover:text-pink transition-color'>Spacecraft</p>
+          <p className='hover:before:[content:"🡆"] hover:before:opacity-100 before:opacity-0 before:w-8 before:inline-block text-white before:transition-all before:relative before:right-10 hover:before:right-0 after:block after:h-1 after:w-3/4 after:bg-white hover:text-pink transition-color'>Destinations</p>
+          <p className='hover:before:[content:"🡆"] hover:before:opacity-100 before:opacity-0 before:w-8 before:inline-block text-white before:transition-all before:relative before:right-10 hover:before:right-0 after:block after:h-1 after:w-3/4 after:bg-white hover:text-pink transition-color'>Training</p>
+          <p className='hover:before:[content:"🡆"] hover:before:opacity-100 before:opacity-0 before:w-8 before:inline-block text-white before:transition-all before:relative before:right-10 hover:before:right-0 after:block after:h-1 after:w-3/4 after:bg-white hover:text-pink transition-color'>Safety</p>
+          <p className='hover:before:[content:"🡆"] hover:before:opacity-100 before:opacity-0 before:w-8 before:inline-block text-white before:transition-all before:relative before:right-10 hover:before:right-0 after:block after:h-1 after:w-3/4 after:bg-white hover:text-pink transition-color'>About</p>
         </div>
       </div>
     </>
