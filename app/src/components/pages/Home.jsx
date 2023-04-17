5231b4b3534ae340214d6453ee6571ae98bf5497
Jakob Nacanaynay <jnac8080@gmail.com>
2023-04-17 09:36:08 -0700

    Shading and get rid of image gradient

--- app/src/components/pages/Home.jsx
+++ app/src/components/pages/Home.jsx
@@ -12,12 +12,12 @@ const Home = () => {
         <HamburgerMenu homeLink={"#root"} goLabel="FLY WITH US" />
         <VideoSlide file="https://svs.gsfc.nasa.gov/vis/a010000/a013500/a013537/13537_Apollo13ViewsMoon4K_HDversion.webm" altimg={peanuts} main="ASTRAL" tagline="TOUCH THE STARS" position="bottomLeft"/>
         <TopMenu homeLink={"#root"} goLabel="FLY WITH US" />
-        <BackgroundImageSlide main="SPACESHIPS" sub="Ride in Comfort" label="LEARN MORE" link="/ships" background={`linear-gradient(rgba(0,0,0,0) 98%, black 101%), url(${rocket1}`} position="bottomLeft" />
-        <BackgroundImageSlide main="DESTINATIONS" label="LEARN MORE" link="/" background={`linear-gradient(rgba(0,0,0,0) 98%, black 101%), url(${moon1}`} position="topLeft" description="A short description, no longer than a paragraph"/>
+        <BackgroundImageSlide main="SPACESHIPS" sub="Ride in Comfort" label="LEARN MORE" link="/ships" background={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${rocket1}`} position="bottomLeft" />
+        <BackgroundImageSlide main="DESTINATIONS" label="LEARN MORE" link="/" background={`url(${moon1}`} position="topLeft" description="A short description, no longer than a paragraph"/>
         <QuoteSlide quote="Astral lies at the bleeding edge of innovation, bringing science fiction to life." author="Aaron Donson" title="Former NASA engineer and reporter for the Springfield Inquirer" image={peanuts} header="A Revolution in Travel"/>
-        <BackgroundImageSlide main="TRAINING" label="LEARN MORE" link="/" background={`linear-gradient(black -1%, rgba(0,0,0,0) 2%, rgba(0,0,0,0) 98%, black 101%), url(${peanuts}`} position="topLeft" description="A short description, no longer than 2 sentences." />
-        <BackgroundImageSlide main="SAFETY" sub="Rest Easily In Our Craft" label="LEARN MORE" link="/" background={`linear-gradient(rgba(0,0,0,0) 98%, black 101%), url(${peanuts}`} position="bottomLeft" />
-        <BackgroundImageSlide main="ABOUT US" sub="You Are Our Mission" label="LEARN MORE" link="/" background={`linear-gradient(rgba(0,0,0,0) 98%, black 101%), url(${people1}`} position="bottomLeft" />
+        <BackgroundImageSlide main="TRAINING" label="LEARN MORE" link="/" background={`url(${peanuts}`} position="topLeft" description="A short description, no longer than 2 sentences." />
+        <BackgroundImageSlide main="SAFETY" sub="Rest Easily In Our Craft" label="LEARN MORE" link="/" background={`url(${peanuts}`} position="bottomLeft" />
+        <BackgroundImageSlide main="ABOUT US" sub="You Are Our Mission" label="LEARN MORE" link="/" background={`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${people1}`} position="bottomLeft" />
     </>
     );
 };
