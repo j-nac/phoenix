8157101b8dcdf79cb568ab5e7d886e34831629e4
Asher-W <85578685+Asher-W@users.noreply.github.com>
2023-05-17 09:10:51 -0700

    Update Destinations.jsx

--- app/src/components/pages/Destinations.jsx
+++ app/src/components/pages/Destinations.jsx
@@ -1,7 +1,17 @@
+import "../../styles/home.css";
+import peanuts from "../../images/roasted-peanuts-jpg-1656535069.jpg";
+import { VideoSlide, BackgroundImageSlide, QuoteSlide  } from "../organisms/Slides";
+import { TopMenu, HamburgerMenu, Footer } from "../organisms/NavElements";
 const Destinations = () => {
-    return <h1 className="text-3xl font-bold underline">
-      This is the destinations page
-    </h1>;
+    return (
+      <>
+        <HamburgerMenu homeLink={"#root"} goLabel="FLY WITH US" />
+        <VideoSlide file="https://svs.gsfc.nasa.gov/vis/a010000/a013500/a013537/13537_Apollo13ViewsMoon4K_HDversion.webm" fit="menu" position="bottomLeft" main="DESTINATIONS" tagline="OH THE PLACES YOU'LL GO"/>
+        <TopMenu homeLink={"#root"} goLabel="FLY WITH US" />
+        <BackgroundImageSlide main="SPACESHIPS" sub="Ride in Comfort" background={`url(${peanuts}`} position="bottomLeft" />
+        
+      </>
+    )
 };
   
 export default Destinations;
