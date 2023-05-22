ec054dc0e1d44e29eaf8a77a3699a0b049a2a86d
Jakob Nacanaynay <jnac8080@gmail.com>
2023-05-21 21:22:45 -0700

    Fixxx

--- app/src/components/pages/Home.jsx
+++ app/src/components/pages/Home.jsx
@@ -13,11 +13,11 @@ const Home = () => {
         <HamburgerMenu homeLink={"#root"} goLabel="FLY WITH US" />
         <VideoSlide file="https://svs.gsfc.nasa.gov/vis/a010000/a013500/a013537/13537_Apollo13ViewsMoon4K_HDversion.webm" altimg={peanuts} main="ASTRAL" tagline="TOUCH THE STARS" position="bottomLeft" fit="menu"/>
         <TopMenu homeLink={"#root"} goLabel="FLY WITH US" />
-        <BackgroundImageSlide main="SPACESHIPS" sub="Ride in Comfort" label="LEARN MORE" link="/ships" background={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${rocket1}`} position="bottomLeft" />
-        <BackgroundImageSlide main="DESTINATIONS" label="LEARN MORE" link="/destinations" background={`url(${moon1}`} position="topLeft" description="See the moon closer than ever before with our first-class lunar experience"/>
+        <BackgroundImageSlide main="SPACESHIPS" sub="Ride in Comfort" label="LEARN MORE" link="/#/ships" background={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${rocket1}`} position="bottomLeft" />
+        <BackgroundImageSlide main="DESTINATIONS" label="LEARN MORE" link="/#/destinations" background={`url(${moon1}`} position="topLeft" description="See the moon closer than ever before with our first-class lunar experience"/>
         <QuoteSlide quote="Astral lies at the bleeding edge of innovation, bringing science fiction to life." author="Aaron Donson" title="Former NASA engineer and technology editor of the Springfield Inquirer" image={peanuts} header="A Revolution in Travel"/>
-        <BackgroundImageSlide main="TRAINING" label="LEARN MORE" link="/training" background={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${training1}`} position="topLeft" description="Common diving training in our world class facility in Telluride, CO" />
-        <BackgroundImageSlide main="ABOUT US" sub="You Are Our Mission" label="LEARN MORE" link="/about" background={`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${people1}`} position="bottomLeft" />
+        <BackgroundImageSlide main="TRAINING" label="LEARN MORE" link="/#/training" background={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${training1}`} position="topLeft" description="Common diving training in our world class facility in Telluride, CO" />
+        <BackgroundImageSlide main="ABOUT US" sub="You Are Our Mission" label="LEARN MORE" link="/#/about" background={`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${people1}`} position="bottomLeft" />
         <Footer />
     </>
     );
--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Layout from "./components/pages/Layout";
 import About from "./components/pages/About";
 import Booking from "./components/pages/Booking";
@@ -13,20 +13,20 @@ import "./index.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="about" element={<About />} />
           <Route path="booking" element={<Booking />} />
           <Route path="destinations" element={<Destinations />} />
-          <Route index element={<Home />} />
           <Route path="info" element={<Info />} />
           <Route path="*" element={<NoPage />} />
           <Route path="ships" element={<Ships />} />
           <Route path="training" element={<Training />} />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
