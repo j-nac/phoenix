f1957615368e36e860712865dcb1a68e514794ad
Asher-W <85578685+Asher-W@users.noreply.github.com>
2023-05-22 07:12:21 -0700

    Update NavElements.jsx

--- app/src/components/organisms/NavElements.jsx
+++ app/src/components/organisms/NavElements.jsx
@@ -34,12 +34,12 @@ function HamburgerMenu(props){
     <>
         <div className='fixed w-screen h-12 z-50 flex justify-between flex-none px-16 top-2 min-w-slide md:hidden block'>
             <HamburgerIcon openMenu={clicked}  />
-            <TopMenuSelect label={props.goLabel} link="/" />
+            <TopMenuSelect label={props.goLabel} link="/#/booking" />
         </div>
         <div className='fixed h-screen w-screen bg-black min-w-slide z-20 backdrop-blur-sm bg-opacity-75 closed md:hidden' id="menu-back" onClick={clicked}> </div>
         <div className='fixed h-screen w-1/3 bg-gradient-to-t from-black to-purple z-30 flex justify-end min-w-menu closed md:hidden' id="menu">
             <div className='h-1/2 flex justify-between flex-col w-3/4 mt-16'>
-                <MenuSelectPage label="HOME" link="/"/>
+                <MenuSelectPage label="HOME" link="/#/"/>
                 <MenuSelectPage label="SPACESHIPS" link="/#/ships"/>
                 <MenuSelectPage label="DESTINATIONS" link="/#/destinations" />
                 <MenuSelectPage label="TRAINING" link="/#/training" />
@@ -57,7 +57,7 @@ function Footer(props){
             <div className="grow sm:ml-8 p-4 flex flex-col items-center justify-around">
                 <Socials socials={{"https://www.facebook.com/":facebook,"https://www.instagram.com/":instagram, "https://twitter.com/":twitter, "https://www.youtube.com/":youtube}} />
                 <div className="flex justify-center flex-wrap w-full lg:w-3/4">
-                    <TopMenuSelect label="HOME" link="/" mx="1" />
+                    <TopMenuSelect label="HOME" link="/#/" mx="1" />
                     <TopMenuSelect label="SPACESHIPS" link="/#/ships" mx="1" />
                     <TopMenuSelect label="DESTINATIONS" link="/#/destinations" mx="1" />
                     <TopMenuSelect label="TRAINING" link="/#/training" mx="1" />
