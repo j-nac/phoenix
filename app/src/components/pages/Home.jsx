d3b3de5234fbef11cd623d0cd55cb23f3fd53ee6
Jakob Nacanaynay <jnac8080@gmail.com>
2024-04-26 20:49:42 +0000

    Fix more links

--- app/src/components/pages/Home.jsx
+++ app/src/components/pages/Home.jsx
@@ -17,11 +17,11 @@ const Home = () => {
         <HamburgerMenu homeLink={"#root"} goLabel="FLY WITH US" />
         <VideoSlide file={spacemont} main="ASTRAL" tagline="TOUCH THE STARS" position="bottomLeft" fit="menu"/>
         <TopMenu homeLink={"#root"} goLabel="FLY WITH US" />
-        <BackgroundImageSlide main="SPACESHIPS" sub="Ride in Comfort" label="LEARN MORE" link="/#/ships" background={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${rocket1}`} position="bottomLeft" />
-        <BackgroundImageSlide main="DESTINATIONS" label="LEARN MORE" link="/#/destinations" background={`url(${moon1}`} position="topLeft" description="See the moon closer than ever before with our first-class lunar experience"/>
+        <BackgroundImageSlide main="SPACESHIPS" sub="Ride in Comfort" label="LEARN MORE" link="/phoenix/#/ships" background={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${rocket1}`} position="bottomLeft" />
+        <BackgroundImageSlide main="DESTINATIONS" label="LEARN MORE" link="/phoenix/#/destinations" background={`url(${moon1}`} position="topLeft" description="See the moon closer than ever before with our first-class lunar experience"/>
         <QuoteSlide quote="Astral lies at the bleeding edge of innovation, bringing science fiction to life." author="Aaron Donson" title="Former NASA engineer and technology editor of the Springfield Inquirer" image={blah} header="A Revolution in Travel"/>
-        <BackgroundImageSlide main="TRAINING" label="LEARN MORE" link="/#/training" background={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${training1}`} position="topLeft" description="Common diving training in our world class facility in Telluride, CO" />
-        <BackgroundImageSlide main="ABOUT US" sub="You Are Our Mission" label="LEARN MORE" link="/#/about" background={`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${people1}`} position="bottomLeft" />
+        <BackgroundImageSlide main="TRAINING" label="LEARN MORE" link="/phoenix/#/training" background={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${training1}`} position="topLeft" description="Common diving training in our world class facility in Telluride, CO" />
+        <BackgroundImageSlide main="ABOUT US" sub="You Are Our Mission" label="LEARN MORE" link="/phoenix/#/about" background={`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${people1}`} position="bottomLeft" />
         <Footer />
     </>
     );
