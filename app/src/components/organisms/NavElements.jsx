fa39067b498786fb4dae77b5251cd5cee1f4625f
Jakob Nacanaynay <jnac8080@gmail.com>
2024-04-26 16:22:43 +0000

    Finally fix links

--- app/src/components/organisms/NavElements.jsx
+++ app/src/components/organisms/NavElements.jsx
@@ -13,12 +13,12 @@ import $ from "jquery";
 function TopMenu(props) {
     return (
         <div className="sticky top-0 h-menu bg-black-glass z-50 items-end justify-around hidden md:flex backdrop-blur-md flex items-center">
-            <LogoSelect label="LOGO HERE" link="/" />
-            <TopMenuSelect label="SPACESHIPS" link="/ships" />
-            <TopMenuSelect label="DESTINATIONS" link="/destinations" />
-            <TopMenuSelect label="TRAINING" link="/training" />
-            <TopMenuSelect label="ABOUT" link="/about" />
-            <TopMenuSelect label="FLY WITH US" link="/booking" />
+            <LogoSelect label="LOGO HERE" link="/phoenix" />
+            <TopMenuSelect label="SPACESHIPS" link="/phoenix/ships" />
+            <TopMenuSelect label="DESTINATIONS" link="/phoenix/destinations" />
+            <TopMenuSelect label="TRAINING" link="/phoenix/training" />
+            <TopMenuSelect label="ABOUT" link="/phoenix/about" />
+            <TopMenuSelect label="FLY WITH US" link="/phoenix/booking" />
         </div>
     )
 }
@@ -34,16 +34,16 @@ function HamburgerMenu(props){
     <>
         <div className='fixed w-screen h-12 z-50 flex justify-between flex-none px-16 top-2 min-w-slide md:hidden block'>
             <HamburgerIcon openMenu={clicked}  />
-            <TopMenuSelect label={props.goLabel} link="/booking" />
+            <TopMenuSelect label={props.goLabel} link="/phoenix/booking" />
         </div>
         <div className='fixed h-screen w-screen bg-black min-w-slide z-20 backdrop-blur-sm bg-opacity-75 closed md:hidden' id="menu-back" onClick={clicked}> </div>
         <div className='fixed h-screen w-1/3 bg-gradient-to-t from-black to-purple z-30 flex justify-end min-w-menu closed md:hidden' id="menu">
             <div className='h-1/2 flex justify-between flex-col w-3/4 mt-16'>
-                <MenuSelectPage label="HOME" link="/"/>
-                <MenuSelectPage label="SPACESHIPS" link="/ships"/>
-                <MenuSelectPage label="DESTINATIONS" link="/destinations" />
-                <MenuSelectPage label="TRAINING" link="/training" />
-                <MenuSelectPage label="ABOUT" link="/about" />
+                <MenuSelectPage label="HOME" link="/phoenix"/>
+                <MenuSelectPage label="SPACESHIPS" link="/phoenix/ships"/>
+                <MenuSelectPage label="DESTINATIONS" link="/phoenix/destinations" />
+                <MenuSelectPage label="TRAINING" link="/phoenix/training" />
+                <MenuSelectPage label="ABOUT" link="/phoenix/about" />
             </div>
         </div>
     </>
@@ -57,12 +57,12 @@ function Footer(props){
             <div className="grow sm:ml-8 p-4 flex flex-col items-center justify-around">
                 <Socials socials={{"https://www.facebook.com/":facebook,"https://www.instagram.com/":instagram, "https://twitter.com/":twitter, "https://www.youtube.com/":youtube}} />
                 <div className="flex justify-center flex-wrap w-full lg:w-3/4">
-                    <TopMenuSelect label="HOME" link="/" mx="1" />
-                    <TopMenuSelect label="SPACESHIPS" link="/ships" mx="1" />
-                    <TopMenuSelect label="DESTINATIONS" link="/destinations" mx="1" />
-                    <TopMenuSelect label="TRAINING" link="/training" mx="1" />
-                    <TopMenuSelect label="ABOUT" link="/about" mx="1" />
-                    <TopMenuSelect label="BUY NOW" link="/booking" mx="1" />
+                    <TopMenuSelect label="HOME" link="/phoenix" mx="1" />
+                    <TopMenuSelect label="SPACESHIPS" link="/phoenix/ships" mx="1" />
+                    <TopMenuSelect label="DESTINATIONS" link="/phoenix/destinations" mx="1" />
+                    <TopMenuSelect label="TRAINING" link="/phoenix/training" mx="1" />
+                    <TopMenuSelect label="ABOUT" link="/phoenix/about" mx="1" />
+                    <TopMenuSelect label="BUY NOW" link="/phoenix/booking" mx="1" />
                 </div>
             </div>
         </div>
